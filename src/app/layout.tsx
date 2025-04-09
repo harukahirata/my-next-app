9c88a6282b5d906c1f475942743fb39689fcd6e7
harukahirata <haruka.h@chot-inc.com>
2025-04-09 11:49:21 +0900

    test: テスト用コミット

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ja">
       <body>
         <Header />
-        <main>{children}</main>
+        <main>{children} </main>
         <Footer />
       </body>
     </html>
