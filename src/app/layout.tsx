255726b1e4488d9eb8139df743689defdb5e1643
harukahirata <haruka.h@chot-inc.com>
2025-04-10 14:53:06 +0900

    test: 1つ目の変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,23 @@
-import './globals.scss';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import "./globals.scss";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: 'My App',
-  description: '自分で作ったNext.jsアプリ'
-}
+	title: "My App",
+	description: "自分で作ったNext.jsアプリ",
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ja">
-      <body>
-        <Header />
-        <main>{children}　</main>
-        <Footer />
-      </body>
-    </html>
-  )
+export default function RootLayout({
+	children,
+}: { children: React.ReactNode }) {
+	return (
+		<html lang="ja">
+			<body>
+				<Header />
+				<main>{children}　</main>
+				<p>testtest</p>
+				<Footer />
+			</body>
+		</html>
+	);
 }
